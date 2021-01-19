57c1016f0e687c0b88471c5b3d68bf1fe1ed15c4
yao-qiuming <yao.ornl@gmail.com>
2021-01-19 13:01:42 -0500

    Add files via upload

--- Figure1AB/run_crisprsurf.sh
+++ Figure1AB/run_crisprsurf.sh
@@ -1,1 +1,7 @@
-##
+#From Johnason Hsu
+
+#Cas9 screen:
+docker run -v ${PWD}/:/DATA -w /DATA pinellolab/crisprsurf SURF_deconvolution -f [INSERT FILE NAME] -pert cas9 -sim_n 1000 -range 7
+ 
+#dCas9 screen:
+docker run -v ${PWD}/:/DATA -w /DATA pinellolab/crisprsurf SURF_deconvolution -f [INSERT FILE NAME] -pert cas9 -sim_n 1000 -range 20
